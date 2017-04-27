8acd32374c071cb4346cee1c7e9fc11ba5f18c27
amit gal <amitgal@amits-MacBook-Pro-2.local>
2017-04-27 23:49:57 +0300

    event functions added

--- app/src/main/java/com/example/amit/Event.java
+++ app/src/main/java/com/example/amit/Event.java
@@ -15,18 +15,56 @@ public class Event extends RealmObject {
 
     public Group group;
     public double middleLongtitude;
-    public double meddleLatitude;
+    public double middleLatitude;
     public Place meetPoint;
     public RealmList<Place> places;
+    public double radius;
 
 
     public Event(Group group){
         this.group = group;
-        this.meetPoint = getmeetPoint();
-
+        this.middleLongtitude = findMiddleLongtitude();
+        this.middleLatitude = findMiddleLatitude();
+        this.radius = findRadius();
     }
 
-    public Place getmeetPoint() {
-        return meetPoint;
+
+    private double findMiddleLatitude() {
+        double ans = 0;
+        for(Place p : places){
+            ans += p.latitude;
+        }
+        ans /= places.size();
+        return ans;
     }
+
+
+    private double findMiddleLongtitude() {
+        double ans = 0;
+        for(Place p : places){
+            ans += p.longitude;
+        }
+        ans /= places.size();
+        return ans;
+    }
+
+
+    public double findRadius(){
+        double radius = Integer.MIN_VALUE;
+        for(Place p : places){
+            if (radius < computeDistance(p)) {
+                radius = computeDistance(p);
+            }
+        }
+        return radius;
+    }
+
+
+    private double computeDistance(Place p){
+        return Math.sqrt((Math.pow(p.latitude - middleLatitude, 2))
+                + Math.pow(p.longitude - middleLongtitude, 2));
+    }
+
+
+
 }
